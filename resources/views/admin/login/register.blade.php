c2607ae9e733d4b42b174af5d79ea691afbd9101
shimul3200 <shimulnath016@gmail.com>
2019-04-20 17:53:15 +0600

    hghghg

--- resources/views/admin/login/register.blade.php
+++ resources/views/admin/login/register.blade.php
@@ -39,17 +39,33 @@
         <form class="form" method="POST" action="">
             {{ csrf_field() }}
 
-            <div class="form-group has-feedback">
+            <div class="form-group has-feedback {{ $errors->has('name') ? ' has-error' : '' }}">
                 <input type="text" class="form-control" name="name" placeholder="Full name">
                 <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                @if ($errors->has('name'))
+                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                @endif
             </div>
-            <div class="form-group has-feedback">
+            <div class="form-group has-feedback {{ $errors->has('email') ? ' has-error' : '' }}">
                 <input type="email" class="form-control" name="email" placeholder="Email">
                 <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                @endif
             </div>
-            <div class="form-group has-feedback">
+            <div class="form-group has-feedback {{ $errors->has('password') ? ' has-error' : '' }}">
                 <input type="password" class="form-control" name="password" placeholder="Password">
                 <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                @endif
             </div>
             <div class="form-group has-feedback">
                 <input type="password" class="form-control" name="password_confirmation" placeholder="Retype password">
